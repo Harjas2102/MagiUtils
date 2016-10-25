35623a46595e23dfe7501531bee665186304ba07
HeyZeer0 <heyzeer0@yahoo.com.br>
2016-10-25 11:58:21 -0200

    Fixed restart timer

--- src/net/heyzeer0/magi/tasks/RestartTimer.java
+++ src/net/heyzeer0/magi/tasks/RestartTimer.java
@@ -57,6 +57,8 @@ public class RestartTimer {
                     }
 
                     startSecondCountdown();
+                }else{
+                    startRestartCountdown();
                 }
 
             }
